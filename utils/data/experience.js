04c564dc4d050d6ccccac0845a409c7923252f14
Sajad Aliarab <sajad.aliarab@gmail.com>
2024-06-02 16:06:53 +0100

    Change in experience

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -2,37 +2,30 @@ export const experiences = [
  
   {
     id: 1,
-    title: "FullStack Developer ",
+    title: "Web Developer ",
     company: "Butane Industrial Group, Iran",
-    duration: "(Apr 2021 - Jan 2023)",
+    duration: "(Apr 2020 - Dec 2022)",
     image: "/png/butane.png",
   },
   {
     id: 2,
-    title: "Front-End Developer",
-    company: "Butane Industrial Group, Iran",
-    duration: "(Feb 2018 - Apr 2021)",
-    image: "/png/butane.png",
+    title: "Founder and Website Administrator ",
+    company: "DamaNama, Iran",
+    duration: "(Apr 2012 - Apr 2020)",
+    image: "/png/damanama.png",
   },
   {
-    id: 3,
-    title: "Website Administrator ",
-    company: "DamaNama, Iran",
-    duration: "(Apr 2013 - Feb 2018)",
-    image: "/png/damanama.png",
+    id:3,
+    title: "Trainee Web Developer",
+    company: "Javaneh Software, Iran",
+    duration: "(Apr 2009 - Apr 2011)",
+    image: "/png/javaneh.png",
   },
   {
     id: 4,
-    title: "Content Creator",
-    company: "DamaNama, Iran",
-    duration: "(Aug 2011 - Apr 2013)",
-    image: "/png/damanama.png",
-  },
-  {
-    id:5,
-    title: "Trainee Web Developer",
-    company: "Javaneh Software, Iran",
-    duration: "(Sep 2009 - May 2011)",
-    image: "/png/javaneh.png",
+    title: "Technical Sales Assistance",
+    company: "Toolstation , UK",
+    duration: "(May 2023 - Present)",
+    image: "/svg/toolstation.svg"
   }
 ]
