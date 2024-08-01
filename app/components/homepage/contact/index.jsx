e02ab692a8549be08d42002cc2fe319bb3b35727
Sajad Aliarab <sajad.aliarab@gmail.com>
2024-08-01 11:54:19 +0100

    some change in the information 3

--- app/components/homepage/contact/index.jsx
+++ app/components/homepage/contact/index.jsx
@@ -55,7 +55,7 @@ function ContactSection() {
                 size={48}
               />
             </Link>
-            <Link target="_blank" href="https://www.linkedin.com/in/sajad-aliarab-9631607b/">
+            <Link target="_blank" href="https://www.linkedin.com/in/sajad-rab">
               <BiLogoLinkedin
                 className="bg-[#8b98a5] p-3 rounded-full hover:bg-[#16f2b3] hover:scale-110 transition-all duration-300 text-gray-800 cursor-pointer"
                 size={48}
