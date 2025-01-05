1f700f13a600fc6577a764cccd8ac2409ea169ae
Sajad Rab <120600259+SajadAliarab@users.noreply.github.com>
2025-01-05 19:33:52 +0000

    add lcfc to experience

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -2,20 +2,27 @@ export const experiences = [
  
   {
     id: 1,
+    title: "Digital Platforms Specialist",
+    company: "Leicester City Football Club, UK",
+    duration: "(November 2024 - Present)",
+    image: "/png/LCFC(Logo).png",
+  },
+ {
+    id: 2,
     title: "Full Stack Developer On Community Platform",
     company: "PersonalProject, UK",
     duration: "(July 2024 - Present)",
     image: "/png/freelance-work.png",
   },
   {
-    id: 2,
+    id: 3,
     title: "Web Developer",
     company: "Butane Industrial Group, Iran",
     duration: "(Apr 2020 - Dec 2022)",
     image: "/png/butane.png",
   },
   {
-    id: 3,
+    id: 4,
     title: "Founder of E-Commerce ",
     company: "DamaNama, Iran",
     duration: "(Apr 2014 - Apr 2020)",
